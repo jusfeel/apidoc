8a72dfd0b5709005b079128197a9aa5948eea18a
Hao <wanghao@nobleprog.cn>
2026-01-08 03:43:51 +0000

    Remove use of bootstap 3 popovers and tooltips due to security

--- template/src/webpack.config.js
+++ template/src/webpack.config.js
@@ -10,19 +10,19 @@
  */
 
 /* webpack js bundler config file */
-const path = require('path');
-const { EsbuildPlugin } = require('esbuild-loader');
+const path = require("path");
+const { EsbuildPlugin } = require("esbuild-loader");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'main.js'),
+  entry: path.resolve(__dirname, "main.js"),
   // mode is set at runtime
   resolve: {
     alias: {
-      handlebars: 'handlebars/dist/handlebars.min.js',
+      handlebars: "handlebars/dist/handlebars.min.js",
       // use src jquery, not the minified version or it won't be found
-      jquery: 'jquery/src/jquery',
+      jquery: "jquery/src/jquery",
     },
-    extensions: ['.js', '.mjs'],
+    extensions: [".js", ".mjs"],
     // avoid issue with webpack not finding the polyfill
     fallback: {
       util: false,
@@ -32,22 +32,22 @@ module.exports = {
     rules: [
       // expose jquery globally
       {
-        test: require.resolve('jquery'),
-        loader: 'expose-loader',
+        test: require.resolve("jquery"),
+        loader: "expose-loader",
         options: {
-          exposes: ['$', 'jQuery'],
+          exposes: ["$", "jQuery"],
         },
       },
     ],
   },
   output: {
-    filename: 'main.bundle.js',
+    filename: "main.bundle.js",
     // path is set at runtime
   },
   optimization: {
     minimizer: [
       new EsbuildPlugin({
-        target: 'es2015',
+        target: "es2015",
       }),
     ],
   },
